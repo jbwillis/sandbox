525234d4219f286240447aa36a2f0920d619657f
Jacob Willis <jbwillis272@gmail.com>
2020-06-16 12:23:56 -0600

    Use a class

--- cpp/variadic_templates/vtemp_printf.cpp
+++ cpp/variadic_templates/vtemp_printf.cpp
@@ -16,19 +16,36 @@
 
 #include<stdio.h>
 
-
-template <typename... T>
-void enthusiastic_printf(const char* fmt, T... args)
+class emotional_printf
 {
-	std::cout << __PRETTY_FUNCTION__ << "\n";
+public:
+	template <typename... T>
+	void enthusiastic_printf(const char* fmt, T... args)
+	{
+		std::cout << __PRETTY_FUNCTION__ << "\n";
 
-	std::stringstream ss;
-	ss << "Wow!\n" << fmt << "\nThat's pretty neat!";
-	printf(ss.str().c_str(), args...);
-}
+		std::stringstream ss;
+		ss << "Wow!\n" << fmt << "That's pretty neat!\n";
+		printf(ss.str().c_str(), args...);
+	}
+
+	template <typename... T> // must declare T again
+	void bored_printf(const char* fmt, T... args)
+	{
+		std::cout << __PRETTY_FUNCTION__ << "\n";
+
+		std::stringstream ss;
+		ss << "meh\n" << fmt << "I've seen better\n";
+		printf(ss.str().c_str(), args...);
+	}
+};
 
 int main()
 {
-	enthusiastic_printf("Here's an int (%d)\t a str (%s)\t and a float (%e)\t\n",
+	emotional_printf emp = emotional_printf();
+	emp.enthusiastic_printf("Here's an int (%d)\t a str (%s)\t and a float (%e)\t\n",
+			123, "boom", 3.1415);
+
+	emp.bored_printf("Here's an int (%d)\t a str (%s)\t and a float (%e)\t\n",
 			123, "boom", 3.1415);
 }
